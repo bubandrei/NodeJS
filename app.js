2e735ea14322724d9a3782d3b6f68784c5bd8f73
a.bubelev <bubeleu@gmail.com>
2022-05-12 12:37:47 +0300

    promise

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 import fs from 'fs';
 
+////////////////////////////////////////////////////////////////////////////
+//есть 3 файла с числами. Найдите сумму этих чисел и запишите в новый файл.
+// let names = ['num1.txt', 'num2.txt', 'num3.txt'];
+// let files = [];
+// for (let name of names) {
+//     files.push(fs.promises.readFile(name, 'utf8'));
+// }
+// Promise.all(files).then(data => {
+//     let sum = 0;
+//     for (let num of data) {
+//         sum += +num;
+//     }
+//     fs.promises.writeFile('newFile.txt', sum);
+// }).catch(err => {
+//     console.log('win', err);
 
-fs.promises.readFile('file1.txt', 'utf-8').then(data => {
-    let arr = data.split(',');
-    for (let i = 0; i < arr.length; i++) {
-        let nameFile = 'num' + i + '.txt';
-        fs.promises.writeFile(nameFile, Number(arr[i]));
-    }
-}).catch(err => {
-    console.log('Big error');
-})
+// })
+
+///////////////////////////////////////////////////////////////
+// fs.promises.readFile('file1.txt', 'utf-8').then(data => {
+//     let arr = data.split(',');
+//     for (let i = 0; i < arr.length; i++) {
+//         let nameFile = 'num' + i + '.txt';
+//         fs.promises.writeFile(nameFile, Number(arr[i]));
+//     }
+// }).catch(err => {
+//     console.log('Big error');
+// })
 
 
 
