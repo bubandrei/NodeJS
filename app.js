f8019ca2e7eb8b0ec8c647ef291dae6ca749f632
a.bubelev <bubeleu@gmail.com>
2022-05-14 10:05:56 +0300

    server date

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-import http from 'http';
-http.createServer((request, response) => {
-    response.write('text1');
-    response.write('text2');
-    response.write('text3');
-    response.write('I want to move to another country')
-    response.end();
-}).listen(3000)
+import http, { request } from 'http';
+
+//сервер при запросе отдаёт текущее время
+// let date = new Date();
+// http.createServer((request, response) => {
+//     response.setHeader('Content-Type', 'text/html');
+//     response.statusCode = 200;
+//     response.write(`<div>${date}</div>`);
+//     response.end();
+// }).listen(3000);
+
+
+// http.createServer((request, response) => {
+//     response.setHeader('Content-Type', 'text/html');
+//     response.statusCode = 200;
+//     response.write('<b>Hello World!</b>');
+//     response.end();
+// }).listen(3000)
+
+/////////////////////////////////////////////////////////
+// http.createServer((request, response) => {
+//     response.setHeader('Cache-Control', 'no-cache')
+//     response.statusCode = 200;
+//     response.write('Hello');
+//     response.end();
+// }).listen(3000);
+
+
+
+/////////////////////////////////////////////////////////
+// http.createServer((request, response) => {
+//     response.write('text1');
+//     response.write('text2');
+//     response.write('text3');
+//     response.write('I want to move to another country')
+//     response.end();
+// }).listen(3000)
