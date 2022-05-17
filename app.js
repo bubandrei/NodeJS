d36cbb7c04672b649e15ad98a29e3370126bf192
a.bubelev <bubeleu@gmail.com>
2022-05-17 16:30:54 +0300

    app.iuse

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+////////////////////////////////////////////////////////////////
+//app.use
+// let app = express();
+// app.get('/1/', (req, res) => {
+//     res.send('new page 1');
+// });
+// app.get('/2/', (req, res) => {
+//     res.send('page222222222222222222')
+// })
+// app.use((req, res) => {
+//     res.status(404).send('not found');
+// })
+// app.listen(3000, () => {
+//     console.log('running')
+// })
