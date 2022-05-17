82b13b0b6746de5f73101e9a9f8233d0172452ed
a.bubelev <bubeleu@gmail.com>
2022-05-17 13:05:35 +0300

    multiply url

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 import express from "express";
 let app = express();
 
-app.get('/', function (req, res) {
-    res.send('hello world');
-});
-app.listen(3000, function () {
-    console.log('running');
-});
+
+///////////////////////////////////////////////////
+//multiply url
+// app.get('/', function (req, res) {
+//     res.send('hello world');
+// });
+// app.get('/1/', (req, res) => {
+//     res.send('page1');
+// });
+// app.get('/2', (req, res) => {
+//     res.send('page2');
+// });
+// app.get('/3/', (req, res) => {
+//     res.send('page3');
+// });
+// app.listen(3000, function () {
+//     console.log('running');
+// });
