23653da03927f96e2bcb5f415f769bd16a63f55e
a.bubelev <bubeleu@gmail.com>
2022-05-17 09:04:41 +0300

    regular

--- app.js
+++ app.js
@@ -3,26 +3,51 @@ import http from 'http';
 
 ///////////////////////////////////////////////////////////////////////////////
 //stat server
-// http.createServer(async (request, response) => {
-//     if (request.url != '/favicon.ico') {
-//         let text;
-//         let status;
-//         let path = 'root' + request.url;
-//         try {
-//             if ((await fs.promises.stat(path)).isDirectory()) {
-//                 status = 200;
-//                 path += 'index.html';
-//             }
-//             text = await fs.promises.readFile(path, 'utf-8');
-//         } catch (err) {
-//             status = 404;
-//             text = 'page not found';
-//         }
-//         response.writeHead(status, { 'Content-Type': 'text/html' });
-//         response.write(text);
-//         response.end();
-//     }
-// }).listen(3000);
+function getMimeType(path) {
+    let mimes = {
+        html: 'text/html',
+        jpeg: 'image/jpeg',
+        jpg: 'image/jpeg',
+        png: 'image/png',
+        svg: 'image/svg+xml',
+        json: 'application/json',
+        js: 'text/javascript',
+        css: 'text/css',
+        ico: 'image/x-icon',
+    };
+
+    let exts = Object.keys(mimes);
+    let extReg = new RegExp('\\.(' + exts.join('|') + ')$');
+
+    let ext = path.match(extReg)[1];
+
+    if (ext) {
+        return mimes[ext];
+    } else {
+        return 'text/plain';
+    }
+}
+
+http.createServer(async (request, response) => {
+    if (request.url != '/favicon.ico') {
+        let text;
+        let status;
+        let path = 'root' + request.url;
+        try {
+            if ((await fs.promises.stat(path)).isDirectory()) {
+                status = 200;
+                path += 'index.html';
+            }
+            text = await fs.promises.readFile(path, 'utf-8');
+        } catch (err) {
+            status = 404;
+            text = 'page not found';
+        }
+        response.writeHead(status, { 'Content-Type': getMimeType(path) });
+        response.write(text);
+        response.end();
+    }
+}).listen(3000);
 
 // http.createServer(async (request, response) => {
 //     if (request.url != '/favicon.ico') {
