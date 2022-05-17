49ab1d4579e705c7aea4673cc60b49af1b77c2a3
a.bubelev <bubeleu@gmail.com>
2022-05-17 16:39:35 +0300

    status 403

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 import express from 'express';
+let app = express();
+
+
+////////////////////////////////////////////////////////////////
+// status 403
+// app.get('/1/', (req, res) => {
+//     res.send('page12346');
+// });
+// app.get('/2/', (req, res) => {
+//     res.status(403).send('STOP');
+// });
+// app.get((req, res) => {
+//     res.status(404).send('ERROR');
+// })
+// app.listen(3000, () => {
+//     console.log('Running');
+// })
+
+
 
 //////////////////////////////////////////////////////////////
-app.use
-let app = express();
-app.get('/1/', (req, res) => {
-    res.send('new page 1');
-});
-app.get('/2/', (req, res) => {
-    res.send('page222222222222222222')
-})
-app.use((req, res) => {
-    res.status(404).send('not found');
-})
-app.listen(3000, () => {
-    console.log('running')
-})
+// app.use
+// app.get('/1/', (req, res) => {
+//     res.send('new page 1');
+// });
+// app.get('/2/', (req, res) => {
+//     res.send('page222222222222222222')
+// })
+// app.use((req, res) => {
+//     res.status(404).send('not found');
+// })
+// app.listen(3000, () => {
+//     console.log('running')
+// })
