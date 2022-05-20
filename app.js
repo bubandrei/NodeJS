8872b88ce572ca6a88403bc58ccd33049bcbec89
a.bubelev <bubeleu@gmail.com>
2022-05-20 12:26:23 +0300

    content in five variables

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 import expressHandlebars from 'express-handlebars';
 import express from 'express';
-
 const handlebars = expressHandlebars.create({
     defaultLayout: 'main',
     extname: 'hbs'
 });
 let app = express();
-
 app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs');
 
-app.get('/page/:page/', (req, res) => {
-    let num = req.params.page;
-    if (num > 0 && num < 4) {
-        res.render('page' + num);
-    } else {
-        res.status(404).send('not found');
-    }
-})
-app.listen(3000, () => {
-    console.log('Running');
-});
+//////////////////////////////////////////////////////////////////////
+//content in five variables
+// let obj = {
+//     text1: 'aaaaa', text2: 'bbbbb', text3: 'ccccc', text4: 'ddddd', text5: 'eeeee'
+// }
+// app.get('/page/1/', (req, res) => {
+//     res.render('page1', obj);
+// });
+// app.listen(3000, () => {
+//     console.log('Running');
+// });
+
+
+
+
+// app.get('/page/:page/', (req, res) => {
+//     let num = req.params.page;
+//     if (num > 0 && num < 4) {
+//         res.render('page' + num);
+//     } else {
+//         res.status(404).send('not found');
+//     }
+// })
+// app.listen(3000, () => {
+//     console.log('Running');
+// });
