5e72ca2aa26981d574c3a6867eaba483d5ad7a8b
a.bubelev <bubeleu@gmail.com>
2022-05-25 09:37:23 +0300

    context handlebars

--- app.js
+++ app.js
@@ -9,16 +9,31 @@ app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs');
 
 let product = {
-	name: 'prod',
-	cost: 2000
+    name: 'prod',
+    cost: 2000
 }
-
 app.get('/page/1/', (req, res) => {
-    res.render('index', {product});
+    res.render('index', {
+        amount: 10,
+        product: {
+            name: 'prod',
+            cost: 1000,
+            amount: 5
+        }
+    });
 });
 app.listen(3000, () => {
-    console.log('Running');
-})
+        console.log('Running');
+    });
+
+
+
+// app.get('/page/1/', (req, res) => {
+//     res.render('index', {product});
+// });
+// app.listen(3000, () => {
+//     console.log('Running');
+// })
 
 // let products = ['prod1', 'prod2', 'prod3'];
 
