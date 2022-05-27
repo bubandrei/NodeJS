1dd702b296546f1ed634f7a24e5a508880ab22b5
a.bubelev <bubeleu@gmail.com>
2022-05-27 13:28:06 +0300

    Submitting a GET Form in Express

---
+++ app.js
@@ -0,0 +1,27 @@
+import expressHandlebars from 'express-handlebars';
+import express from 'express';
+const handlebars = expressHandlebars.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+let app = express();
+app.engine('hbs', handlebars.engine);
+app.set('view engine', 'hbs');
+
+////////////////////////////////////////////
+//Submitting a GET Form in Express
+// app.get('/', (req, res) => {
+//     res.render('form');
+// });
+
+// app.get('/target/', (req, res) => {
+//     let num1 = +req.query.test1;
+//     let num2 = +req.query.test2;
+//     let num3 = +req.query.test3;
+//     let sum = num1 + num2 + num3;
+//     res.send('result: ' + sum);
+// });
+
+// app.listen(3000, () => {
+//     console.log('Running');
+// })
