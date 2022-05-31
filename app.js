047d3a33dd794c217916dc89945c0ead2979c547
a.bubelev <bubeleu@gmail.com>
2022-05-31 14:47:47 +0300

    session

--- app.js
+++ app.js
@@ -9,11 +9,31 @@ app.use(cookieParser(secret));
 app.use(expressSession({
     secret: secret,
 }));
-app.use(cookieParser(secret));
 
-app.get('/', (req, res) => {
-res.send('August');
-});
-app.listen(3000, ()=>{
-    console.log('Running');
-});
+////////////////////////////////////////////////////
+// session
+// app.get('/save/', (req, res)=>{
+//     req.session.value = '44/77';
+//     res.send('June');
+// });
+// app.get('/read/', (req, res)=>{
+//     let cookSession = req.session.value;
+//     res.send(cookSession);
+// });
+// app.get('/remove/', (req, res)=>{
+//     delete req.session.value;
+//     res.send('August');
+// });
+// app.listen(3000, ()=>{
+//     console.log('Running');
+// });
+
+
+
+//////////////////////////////////////////////////////
+// app.get('/', (req, res) => {
+// res.send('August');
+// });
+// app.listen(3000, ()=>{
+//     console.log('Running');
+// });
