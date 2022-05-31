636fe3244ddd38abf5247f0ad49d2cae65e7a0d0
a.bubelev <bubeleu@gmail.com>
2022-05-31 16:20:18 +0300

    count cookie-session

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import expressSession from 'express-session';
-
 let app = express();
 
 let secret = '44';
@@ -9,6 +8,20 @@ app.use(cookieParser(secret));
 app.use(expressSession({
     secret: secret,
 }));
+
+//////////////////////////////////////////////////////////////
+//count
+// app.get('/', function (req, res) {
+//     // Update views
+//     console.log(req.session.views);
+//     req.session.views = (req.session.views || 0) + 1
+//     // Write response
+//     res.end(req.session.views + ' views')
+// })
+// app.listen(3000, () => {
+//     console.log('Running');
+// });
+
 
 ////////////////////////////////////////////////////
 // session
