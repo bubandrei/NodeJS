becc2d624dbe7d913c5c96bf04399520291e6777
a.bubelev <bubeleu@gmail.com>
2022-05-31 11:42:44 +0300

    cookie(save, read, remove)

--- app.js
+++ app.js
@@ -5,9 +5,31 @@ let app = express();
 let secret = '44';
 app.use(cookieParser(secret));
 
-app.get('/', (req, res)=>{
-    res.send('hello world');
-});
-app.listen(3000, ()=>{
-    console.log('Running');
-});
+///////////////////////////////////////////////////
+//cookie(save, read, remove)
+// app.get('/', (req, res) => {
+//     res.cookie('general', 'june');
+//     res.send('Summer');
+// });
+// app.get('/read/', (req, res) => {
+//     console.log(req.cookies);
+//     res.send('Best Summer');
+// });
+// app.get('/remove/', (req, res) => {
+// res.clearCookie('general');
+// res.send('July');
+// });
+
+// app.listen(3000, () => {
+//     console.log('Running');
+// });
+
+
+//////////////////////////////////////////////////
+//cookie-parsie
+// app.get('/', (req, res)=>{
+//     res.send('hello world');
+// });
+// app.listen(3000, ()=>{
+//     console.log('Running');
+// });
